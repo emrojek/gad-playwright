1d5d637495f8ac1e989cbadb780a5de8a0869e53
emrojek <michal.rojek.it@gmail.com>
2025-10-03 23:19:36 +0200

    feat(login): Implement test for `keep me sign in` login form checkbox

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '../fixtures/user.fixture';
+import * as fs from 'fs';
 
 test.describe('Login Form', () => {
     test.describe('Tests requiring valid user', () => {
@@ -43,6 +44,48 @@ test.describe('Login Form', () => {
 
             await expect(page).toHaveURL('/login/');
             await expect(welcomeMessage).not.toBeVisible();
+        });
+
+        test('should keep user signed in when "keep me signed in" is checked', async ({
+            page,
+            browser,
+            validUser,
+            loginPage,
+        }) => {
+            const logoutButton = loginPage.getLogoutButton();
+            const storageStatePath = 'playwright/.auth/user-session.json';
+
+            try {
+                await loginPage.fillLoginForm({
+                    email: validUser.email,
+                    password: validUser.password,
+                    keepSignIn: true,
+                });
+
+                await Promise.all([
+                    loginPage.clickFormLoginButton(),
+                    expect(logoutButton).toBeVisible(),
+                ]);
+
+                await page.context().storageState({ path: storageStatePath });
+                expect(fs.existsSync(storageStatePath)).toBeTruthy();
+
+                const newContext = await browser.newContext({ storageState: storageStatePath });
+                const newPage = await newContext.newPage();
+                await newPage.goto('/');
+
+                const newDropdownMenu = newPage.locator('[data-testid="btn-dropdown"]');
+                const newPageLogoutButton = newPage.locator('[id="logoutBtn"]');
+                const newUserWelcome = newPage.locator('[id="username"]');
+
+                await newDropdownMenu.hover();
+                await expect(newPageLogoutButton).toBeVisible();
+                await expect(newUserWelcome).toContainText(`Hello ${validUser.firstName}`);
+            } finally {
+                if (fs.existsSync(storageStatePath)) {
+                    fs.unlinkSync(storageStatePath);
+                }
+            }
         });
     });
 
