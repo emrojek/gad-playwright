c93c0e177b7c466f8ee642c04f5d2b4d694ba580
emrojek <michal.rojek.it@gmail.com>
2025-10-24 14:39:08 +0200

    feat(config): Add TypeScript configuration file with strict compiler options, update playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,9 +31,9 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
